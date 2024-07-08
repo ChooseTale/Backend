56598a3d7186b0fa4fa8a28576603c59e6f1ad62
GwonYeong <rnjsdud980@naver.com>
2024-07-09 08:35:32 +0900

    feat: 선택지는 엔딩페이지에 만들어질 수 없음.

--- src/novel-game/choice/services/choice.service.ts
+++ src/novel-game/choice/services/choice.service.ts
@@ -2,16 +2,22 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ChoiceRepositoryInterface } from '../domain/repositories/choice.repository.interface';
 import { CreateChoiceReqDto } from '../applications/controllers/dto/create-choice.dto';
 import { Prisma } from '@prisma/client';
+import { PageService } from '@@src/novel-game/page/services/page.service';
 
 @Injectable()
 export class ChoiceService {
   constructor(
     @Inject('choiceRepositoryInterface')
     private readonly choiceRepository: ChoiceRepositoryInterface,
+    private readonly pageService: PageService,
   ) {}
 
   async getAllByPageId(pageId: number, transaction: Prisma.TransactionClient) {
     return this.choiceRepository.getAllByPageId(pageId, transaction);
+  }
+
+  async getOneById(id: number, transaction: Prisma.TransactionClient) {
+    return this.choiceRepository.getOneById(id, transaction);
   }
 
   async create(
@@ -23,9 +29,20 @@ export class ChoiceService {
       transaction,
     );
 
-    if (pageChoices.length > 4) {
+    if (pageChoices.length > 3) {
       throw new Error('초이스는 4개까지만 생성 가능합니다.');
     }
+
+    const fromPage = await this.pageService.getOneById(
+      createChoiceReqDto.parentPageId,
+      transaction,
+    );
+
+    if (!fromPage) {
+      throw new Error('해당 페이지가 존재하지 않습니다.');
+    }
+
+    fromPage.checkIsEnding();
 
     const choice = await this.choiceRepository.create(
       pageChoices.length + 1,
