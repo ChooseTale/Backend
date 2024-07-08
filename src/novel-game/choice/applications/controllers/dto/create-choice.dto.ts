4cb298be058f47d96a5624dba70c382a2a6fbe11
GwonYeong <rnjsdud980@naver.com>
2024-07-09 08:31:40 +0900

    feat: page validation 추가

--- src/novel-game/choice/applications/controllers/dto/create-choice.dto.ts
+++ src/novel-game/choice/applications/controllers/dto/create-choice.dto.ts
@@ -1,7 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateChoiceReqDto {
+  @IsNotEmpty()
+  @Type(() => Number)
   parentPageId: number;
+
+  @Type(() => Number)
   childPageId?: number;
+
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
 }
 
