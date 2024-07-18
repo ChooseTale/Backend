9f49c53d34109ce9bb9e15031b40ef3fffd842f7
GwonYeong <rnjsdud980@naver.com>
2024-07-18 20:34:31 +0900

    feat: page 생성 테스트코드 작성

--- test/e2e/game-builder/page/page.e2e-spec.ts
+++ test/e2e/game-builder/page/page.e2e-spec.ts
@@ -23,12 +23,41 @@ describe('Test', () => {
   describe('페이지 생성', () => {
     it('should create a page', async () => {
       //when
-      // const { error, statusCode } = await request(app.getHttpServer())
-      //   .post('/game/1/page')
-      //   .send({ title: 'test', parentPageId: 1, description: 'test' });
+      const { error, statusCode } = await request(app.getHttpServer())
+        .post('/game/1/page')
+        .send({
+          title: 'test',
+          parentPageId: 1,
+          description: 'test',
+          isEnding: false,
+          content: 'page content',
+        });
       // //then
-      // expect(statusCode).toBe(201);
-      // expect(error).toBe(false);
+      if (error) {
+        console.log(error);
+      }
+      expect(statusCode).toBe(201);
+      expect(error).toBe(false);
+    });
+
+    describe('페이지 생성 실패', () => {
+      describe('validation', () => {
+        it('page의 content가 3000자를 초과하면 400을 반환한다', async () => {
+          //when
+          const { error, statusCode } = await request(app.getHttpServer())
+            .post('/game/1/page')
+            .send({
+              title: 'test',
+              parentPageId: 1,
+              description: 'test',
+              isEnding: false,
+              content: 'page content'.repeat(1000),
+            });
+          //then
+          expect(statusCode).toBe(400);
+          expect(error).not.toBe(false);
+        });
+      });
     });
   });
 });
