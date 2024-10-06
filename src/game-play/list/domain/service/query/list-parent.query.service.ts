4ecd962c2948d3ebc2feb0d2fb5ac5f64814aa80
gwonyeong <qwe980121@naver.com>
2024-10-06 17:57:53 +0900

    feat: game 목 데이터 추가 및 isPrivate이 true인 경우 노출되지 않도록 추가

--- src/game-play/list/domain/service/query/list-parent.query.service.ts
+++ src/game-play/list/domain/service/query/list-parent.query.service.ts
@@ -1,7 +1,11 @@
 import { Genres, Prisma } from '@prisma/client';
 
 export class ListParentQueryService {
-  constructor(protected readonly query: Prisma.GameFindManyArgs) {}
+  constructor(protected readonly query: Prisma.GameFindManyArgs) {
+    this.query.where = {
+      isPrivate: false,
+    };
+  }
 
   getQuery() {
     return this.query;
