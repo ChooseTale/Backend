29cf1a0a1285627d0d504d38e4882cf0b09129d1
GwonYeong <rnjsdud980@naver.com>
2025-01-22 20:17:42 +0900

    fix: page 수정

--- src/game-builder/page/application/controllers/dto/update-page.dto.ts
+++ src/game-builder/page/application/controllers/dto/update-page.dto.ts
@@ -1,3 +1,4 @@
+import { Transform, Type } from 'class-transformer';
 import { IsBoolean, IsString, Length } from 'class-validator';
 
 export class UpdatePageReqDto {
@@ -5,9 +6,13 @@ export class UpdatePageReqDto {
   @Length(1, 30)
   title: string;
 
+  @Transform(({ value }) => JSON.parse(value))
   contents: {
     content: string;
   }[];
+
+  @IsBoolean()
+  @Transform(({ value }) => JSON.parse(value) === 'true')
   isEnding: boolean;
 }
 
